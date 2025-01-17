cbed91ff6e43360c78dafcf4e140dc227a6cce7a
Vivek-341 <vivekvardhannada@gmail.com>
2025-01-17 22:31:57 +0530

    17-01-2025 Commit
    Added signin and signout in Login page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+declare var handleSignOut: any;
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  userProfile: any;
+
+  ngOnInit() {
+    this.userProfile = JSON.parse(sessionStorage.getItem("loggedInUser")||"");
+  }
+  constructor(private router: Router) { }
+  logout() {
+    handleSignOut();
+    sessionStorage.removeItem("loggedInUser");
+    this.router.navigate(["/login"]).then(() => {
+      window.location.reload();
+    });
+  }
 
 }
