e9b09c3bf0562ea7e97ecf131df6b04051630f6a
Vivek-341 <vivekvardhannada@gmail.com>
2025-01-18 19:29:02 +0530

    18-01-2025 Commit

--- src/app/username-setup/username-setup.component.ts
+++ src/app/username-setup/username-setup.component.ts
@@ -38,16 +38,14 @@ export class UsernameSetupComponent implements OnInit {
     // Arrays of components to build username from
     const adjectives = ['Cool', 'Swift', 'Bright', 'Quick', 'Clever', 'Epic', 'Super', 'Mega', 'Brave', 'Mighty', 'Noble', 'Royal', 'Wise', 'Agile', 'Bold', 'Daring', 'Elite', 'Fierce', 'Grand', 'Hardy', 'Iron', 'Jolly', 'Keen', 'Lively', 'Magic', 'Nimble', 'Prime', 'Rapid', 'Sharp', 'Tough', 'Ultra', 'Vital', 'Wild', 'Young', 'Zealous', 'Alpha', 'Beta', 'Cyber', 'Dark', 'Echo', 'Flash', 'Ghost', 'Hyper', 'Ice', 'Jade', 'Lunar', 'Metal', 'Neo', 'Omega', 'Pixel', 'Quantum', 'Retro', 'Solar', 'Thunder', 'Urban', 'Vapor', 'Wonder', 'Xenon', 'Yonder', 'Zen', 'Astro', 'Blitz', 'Cosmic', 'Delta', 'Energy', 'Fusion', 'Giga', 'Havoc', 'Inferno', 'Jet', 'Kinetic', 'Logic', 'Macro', 'Neon', 'Orbit', 'Plasma', 'Quasar', 'Radiant', 'Sonic', 'Techno', 'Unity', 'Vortex', 'Warp', 'Xeno', 'Yield', 'Zero', 'Apex', 'Burst', 'Chrome', 'Drift', 'Edge', 'Flux', 'Glitch', 'Hack', 'Impact', 'Jolt', 'Kraft', 'Light', 'Mach', 'Nova', 'Onyx', 'Pulse', 'Quest', 'Rush', 'Storm', 'Titan', 'Ultra', 'Void', 'Wave', 'Xray', 'Yang', 'Zeal'];
     const nouns = ['Hawk', 'Wolf', 'Eagle', 'Tiger', 'Ninja', 'Rider', 'Star', 'Hero', 'Dragon', 'Phoenix', 'Lion', 'Panther', 'Falcon', 'Warrior', 'Knight', 'Samurai', 'Ranger', 'Hunter', 'Scout', 'Pilot', 'Captain', 'Master', 'Legend', 'Champion', 'Guardian', 'Sentinel', 'Wizard', 'Mage', 'Sage', 'Oracle', 'Prophet', 'Seeker', 'Runner', 'Racer', 'Drifter', 'Striker', 'Fighter', 'Soldier', 'Commander', 'Chief', 'Leader', 'Pioneer', 'Explorer', 'Voyager', 'Traveler', 'Wanderer', 'Nomad', 'Ranger', 'Tracker', 'Scout', 'Agent', 'Spy', 'Shadow', 'Ghost', 'Phantom', 'Spirit', 'Specter', 'Wraith', 'Demon', 'Angel', 'Saint', 'Knight', 'Paladin', 'Crusader', 'Templar', 'Warrior', 'Berserker', 'Viking', 'Samurai', 'Ninja', 'Assassin', 'Rogue', 'Thief', 'Bandit', 'Pirate', 'Corsair', 'Mariner', 'Captain', 'Admiral', 'General', 'Marshal', 'Emperor', 'King', 'Prince', 'Duke', 'Lord', 'Baron', 'Knight', 'Squire', 'Page', 'Scholar', 'Sage', 'Master', 'Guru', 'Monk', 'Priest', 'Bishop', 'Cardinal', 'Pope', 'Saint', 'Angel', 'Demon', 'Devil', 'Titan', 'Giant', 'Colossus', 'Beast', 'Monster'];
-    
-    // Get random components
     const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
     const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
     const randomNum = Math.floor(Math.random() * 1000);
-    const specialChars = ['@', '$', '_'];
+    const specialChars = ['#', '@', '$', '_'];
     const randomChar = specialChars[Math.floor(Math.random() * specialChars.length)];
-    const randomUpperCase = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    
-    this.generatedUsername = `${randomUpperCase}${name}${randomChar}${randomNum}`;
+
+    // Combine components to create username
+    this.generatedUsername = `${randomAdjective}${randomNoun}${randomChar}${randomNum}`;
     this.usernameForm.patchValue({ username: this.generatedUsername });
   }
 
